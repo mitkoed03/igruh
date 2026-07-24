3cbe5ee8e796e07873425a2d979d5c6ce441316a
Momchil Mladenov <bugybon1@gmail.com>
2026-07-24 13:46:40 +0300

    Proper separate dialogue system in old game
    myDialogue is working as intended now

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,3 +6,8 @@ func add_point():
 	score += 1
 	Event.emit_signal("_change_score",score)
 	print(score)
+
+@export var dialogue_resource: DialogueResource
+
+func _ready() -> void:
+	DialogueManager.show_dialogue_balloon_scene(preload("res://scenes/myDialogue.tscn"), dialogue_resource, "start")
---
+++ scripts/text_container.gd
@@ -0,0 +1,23 @@
+extends VBoxContainer
+
+### Emitted when typing starts
+#signal started_typing()
+#
+### Emitted when typing finishes.
+#signal finished_typing()
+
+var previous_text = ""
+@onready var history_label: RichTextLabel = %HistoryLabel
+@onready var character_label: RichTextLabel = %CharacterLabel
+@onready var dialogue_label: DialogueLabel = %DialogueLabel
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	dialogue_label.started_typing.connect(on_started_typing)
+	dialogue_label.finished_typing.connect(on_finished_typing)
+	
+func on_started_typing() -> void:
+	history_label.text += previous_text
+	
+func on_finished_typing() -> void:
+	previous_text = "[color=#aaaaaa]" + character_label.text  + "[/color]" + '\n' + dialogue_label.text + "\n\n"
